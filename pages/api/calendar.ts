70afbb79fcacf58919a596529732652273ebc392
Emma Adams <emma@adams.engineer>
2022-06-28 15:34:18 -0700

    Add initThinBackend to calendar endpoint

--- pages/api/calendar.ts
+++ pages/api/calendar.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import ical from "ical-generator";
+import { initThinBackend } from "thin-backend";
+
+initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 // TODO: basically need to create a separate public entities
 // TODO: Set an api key for accessing this endpoint
