e43d77115be89f5e7b1f2715bcf43755ffcfe872
Emma Adams <emma@adams.engineer>
2022-06-28 15:48:10 -0700

    Query public notes in the endpoint

--- pages/api/calendar/[secret].ts
+++ pages/api/calendar/[secret].ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import ical from "ical-generator";
 import { initThinBackend } from "thin-backend";
+import { query } from "thin-backend";
 
 initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
@@ -11,6 +12,17 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
+  const { secret } = req.query;
+
+  if (!secret || typeof secret !== "string") {
+    res.status(400).send("Missing secret");
+    return;
+  }
+
+  // TODO: For now secret is just user id, but it should be something separate and resettable.
+  let publicNotes = query("public_notes").where("userId", secret).fetch();
+  console.log(JSON.stringify(publicNotes)); // TODO: actually create stuff from this
+
   const calendar = ical({ name: "my first iCal" });
   const startTime = new Date();
   const endTime = new Date();
