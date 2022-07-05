e51bb3c108d2b3daceef148a738a7944be3199ba
Emma Adams <emma@adams.engineer>
2022-07-05 09:27:09 -0700

    Close menu when clicking on a menu item Remove mobile menu code

--- lib/utils/navigation/AppBar.tsx
+++ lib/utils/navigation/AppBar.tsx
@@ -5,20 +5,17 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import InputBase from '@mui/material/InputBase'
-import Badge from '@mui/material/Badge'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
 import SearchIcon from '@mui/icons-material/Search'
-import AccountCircle from '@mui/icons-material/AccountCircle'
-import MailIcon from '@mui/icons-material/Mail'
-import NotificationsIcon from '@mui/icons-material/Notifications'
 import { AppProps } from '../../../pages/_app'
 import { logout } from 'thin-backend'
 import { useCurrentUser } from 'thin-backend-react'
 import { createRecord } from 'thin-backend'
 import { useRouter } from 'next/router'
 import { parseSearchQuery } from '../../search'
+import Link from 'next/link'
 
 const Search = styled('div')(({ theme }) => ({
   'position': 'relative',
@@ -75,13 +72,8 @@ export function PrimaryAppBar({
     setAnchorEl(event.currentTarget)
   }
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null)
-  }
-
   const handleMenuClose = () => {
     setAnchorEl(null)
-    handleMobileMenuClose()
   }
 
   const menuId = 'primary-search-account-menu'
@@ -106,71 +98,21 @@ export function PrimaryAppBar({
       <MenuItem>--</MenuItem>
       <MenuItem
         onClick={() => {
-          router.push(`/`) // TODO: DO not do this imperatively!! just link
+          handleMenuClose()
         }}
       >
-        Notes
+        <Link href="/">Notes</Link>
       </MenuItem>
       <MenuItem
         onClick={async () => {
+          // TODO: Create a url that is `/notes/new` that does this by default so anywhere can link to it without all this code
           // TODO: Figure out why textSearch and error need to be set to null for new notes?
           const note = await createRecord('notes', {} as any)
+          handleMenuClose()
           router.push(`/notes/${note.id}`)
         }}
       >
         Create New Note
-      </MenuItem>
-    </Menu>
-  )
-
-  const mobileMenuId = 'primary-search-account-menu-mobile'
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>{useCurrentUser()?.email}</p>
       </MenuItem>
     </Menu>
   )
@@ -206,7 +148,6 @@ export function PrimaryAppBar({
           </Search>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
       {renderMenu}
       <br />
       <br />
