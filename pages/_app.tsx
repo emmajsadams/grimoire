c586d6cde7de46813918540b8adf2d567056a342
Emma Adams <emma@adams.engineer>
2022-11-29 20:01:45 -0800

    Setup prisma db and github auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThinBackend } from 'thin-backend-react'
 import { Container } from '@mui/material'
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { SessionProvider } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 
 import { PrimaryAppBar } from 'lib/navigation'
 import { Query, parseSearchQuery } from 'lib/search'
@@ -15,6 +17,29 @@ export interface AppProps {
   clientId: string
   searchQuery: Query
   setSearchQuery: (query: Query) => void
+}
+
+// TODO: redirect unauthenticated, and consider handling loading state in component
+// TODO: use auth url
+function LoginContainer({ children }: any): JSX.Element {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    return <>Loading</>
+  }
+
+  if (status === 'unauthenticated') {
+    return (
+      <>
+        <a href="/api/auth/signin">Login to access Grimoire</a>
+      </>
+    )
+  }
+
+  console.log(session?.user?.email)
+  console.log(session?.user?.name)
+
+  return children
 }
 
 // TODO: What is the accurate type here?
@@ -30,16 +55,20 @@ function MyApp({ Component, pageProps }: any): JSX.Element {
   }
 
   return (
-    <ThinBackend requireLogin>
-      <Head>
-        <meta name="description" content="Grimoire Automata" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <PrimaryAppBar {...customProps} />
-      <Container maxWidth="lg">
-        <Component {...customProps} {...pageProps} />
-      </Container>
-    </ThinBackend>
+    <SessionProvider session={pageProps.session}>
+      <LoginContainer>
+        <ThinBackend requireLogin>
+          <Head>
+            <meta name="description" content="Grimoire Automata" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <PrimaryAppBar {...customProps} />
+          <Container maxWidth="lg">
+            <Component {...customProps} {...pageProps} />
+          </Container>
+        </ThinBackend>
+      </LoginContainer>
+    </SessionProvider>
   )
 }
 export default MyApp
