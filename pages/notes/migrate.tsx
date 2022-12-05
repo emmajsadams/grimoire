b017ddefbc389eb6044934c46fb064fdfa1d2544
Emma Adams <emma@adams.engineer>
2022-12-05 11:23:21 -0800

    Comment out migrate

--- pages/notes/migrate.tsx
+++ pages/notes/migrate.tsx
@@ -1,111 +1,111 @@
-import prisma from 'lib/prisma'
+// import prisma from 'lib/prisma'
 
-import postgres from 'postgres'
-import { diffString, diff } from 'json-diff'
+// import postgres from 'postgres'
+// import { diffString } from 'json-diff'
 
-const MigrateView: any = (props) => {
-  const { postgresNotes, thinNotes } = props
+// const MigrateView: any = (props) => {
+//   const { postgresNotes, thinNotes } = props
 
-  let postgresNotesElement = <></>
-  if (postgresNotes && postgresNotes.length > 0) {
-    postgresNotesElement = (
-      <>
-        <p>Postgres Notes</p>
-        <ul>
-          {postgresNotes.map((note: any) => (
-            <li key={note.title}>{JSON.stringify(note)}</li>
-          ))}
-        </ul>
-      </>
-    )
-  }
+//   let postgresNotesElement = <></>
+//   if (postgresNotes && postgresNotes.length > 0) {
+//     postgresNotesElement = (
+//       <>
+//         <p>Postgres Notes</p>
+//         <ul>
+//           {postgresNotes.map((note: any) => (
+//             <li key={note.title}>{JSON.stringify(note)}</li>
+//           ))}
+//         </ul>
+//       </>
+//     )
+//   }
 
-  let thinNotesElement = <></>
-  if (thinNotes && thinNotes.length > 0) {
-    thinNotesElement = (
-      <>
-        <p>Thin Notes</p>
-        <ul>
-          {thinNotes.map((note: any) => (
-            <li key={note.title}>{JSON.stringify(note)}</li>
-          ))}
-        </ul>
-      </>
-    )
-  }
+//   let thinNotesElement = <></>
+//   if (thinNotes && thinNotes.length > 0) {
+//     thinNotesElement = (
+//       <>
+//         <p>Thin Notes</p>
+//         <ul>
+//           {thinNotes.map((note: any) => (
+//             <li key={note.title}>{JSON.stringify(note)}</li>
+//           ))}
+//         </ul>
+//       </>
+//     )
+//   }
 
-  console.log(diffString(postgresNotes, thinNotes))
+//   console.log(diffString(postgresNotes, thinNotes))
 
-  return (
-    <>
-      {postgresNotesElement}
-      <br />
-      {thinNotesElement}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {postgresNotesElement}
+//       <br />
+//       {thinNotesElement}
+//     </>
+//   )
+// }
 
-export async function getServerSideProps() {
-  await prisma.note.deleteMany({})
+// export async function getServerSideProps() {
+//   await prisma.note.deleteMany({})
 
-  const sql = postgres(process.env.THIN_DB_URL || '', {})
-  let thinNotes = await sql`
-      select
-        id,
-        title,
-        due,
-        status,
-        all_day,
-        description
-      from notes
-      where status != 'deleted'
-        AND status != 'done'
-        AND title != ''
-    `
-  for (const note of thinNotes) {
-    delete note.id
+//   const sql = postgres(process.env.THIN_DB_URL || '', {})
+//   let thinNotes = await sql`
+//       select
+//         id,
+//         title,
+//         due,
+//         status,
+//         all_day,
+//         description
+//       from notes
+//       where status != 'deleted'
+//         AND status != 'done'
+//         AND title != ''
+//     `
+//   for (const note of thinNotes) {
+//     delete note.id
 
-    note.title = note.title.trim()
-    note.description = note.description.trim()
+//     note.title = note.title.trim()
+//     note.description = note.description.trim()
 
-    if (note.due) {
-      note.due = note.due.toISOString() as any
-    } else {
-      delete note.due
-    }
+//     if (note.due) {
+//       note.due = note.due.toISOString() as any
+//     } else {
+//       delete note.due
+//     }
 
-    note.allDay = note.all_day
-    delete note.all_day
-  }
+//     note.allDay = note.all_day
+//     delete note.all_day
+//   }
 
-  await prisma.note.createMany({
-    data: thinNotes,
-    skipDuplicates: true,
-  })
+//   await prisma.note.createMany({
+//     data: thinNotes,
+//     skipDuplicates: true,
+//   })
 
-  const postgresNotes = await prisma.note.findMany({
-    where: {
-      NOT: {
-        status: 'deleted',
-      },
-    },
-  })
-  for (const note of postgresNotes) {
-    delete note.id
-    if (note.due) {
-      note.due = note.due.toISOString() as any
-    } else {
-      delete note.due
-    }
-    delete note.version // note nneeded for repari
-  }
+//   const postgresNotes = await prisma.note.findMany({
+//     where: {
+//       NOT: {
+//         status: 'deleted',
+//       },
+//     },
+//   })
+//   for (const note of postgresNotes) {
+//     delete note.id
+//     if (note.due) {
+//       note.due = note.due.toISOString() as any
+//     } else {
+//       delete note.due
+//     }
+//     delete note.version // note nneeded for repari
+//   }
 
-  return {
-    props: {
-      postgresNotes,
-      thinNotes,
-    },
-  }
-}
+//   return {
+//     props: {
+//       postgresNotes,
+//       thinNotes,
+//     },
+//   }
+// }
 
-export default MigrateView
+// export default MigrateView
